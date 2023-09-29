02e7f017b584fb07a7bd6f01b9ed1bb27f732f49
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-09-29 17:53:38 +0300

    Adding security groups

---
+++ security_group1.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "ssh_sg" {
+  name        = "allow-ssh"
+  description = "Allow ssh access from anywhere"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg-tf"
+  }
+}
---
+++ security_group2.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "ssh_port3000_sg" {
+  name        = "allow-ssh-and-port3000"
+  description = "Allow SSH and Port 3000 traffic from VPC CIDR only"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.myvpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.myvpc.cidr_block]
+  }
+
+  tags = {
+    Name = "ssh-port3000-sg-tf"
+  }
+}
