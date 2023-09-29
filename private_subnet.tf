2b674800e416456196fd99415a78d29569887eeb
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-09-29 17:06:41 +0300

    edit route tables associations

--- private_subnet.tf
+++ private_subnet.tf
@@ -9,7 +9,6 @@ resource "aws_subnet" "private_subnet_az1" {
 }
 
 resource "aws_subnet" "private_subnet_az2" {
-  count             = 1
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
