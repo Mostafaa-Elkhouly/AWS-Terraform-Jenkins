1790027618c309bed57281282657b16f28e63c3e
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-09-29 15:23:38 +0300

    edit route tables

--- private_route.tf
+++ private_route.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "myrt" {
+resource "aws_route_table" "myrtb-private" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
@@ -9,13 +9,4 @@ resource "aws_route_table" "myrt" {
   tags = {
     Name = "private-route-tf"
   }
-}
-
-data "aws_ec2_local_gateway_route_table" "myrt-1" {
-  outpost_arn = "arn:aws:outposts:us-west-2:123456789012:outpost/op-1234567890abcdef"
-}
-
-resource "aws_ec2_local_gateway_route_table_vpc_association" "vpc-tr-1" {
-  local_gateway_route_table_id = data.aws_ec2_local_gateway_route_table.myrt-1.id
-  vpc_id                       = aws_vpc.myvpc-1.id
 }
