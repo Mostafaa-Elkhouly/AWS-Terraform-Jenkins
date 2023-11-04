acf8d6c5fb6810492ac5f1cc63023ad8ea59bc4c
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-11-04 09:23:22 +0200

    Event notification trigger changes at s3 bucket

---
+++ eventnotification.tf
@@ -0,0 +1,12 @@
+# create an event notification that trigger changes at s3 bucket and use lambda function to sent mail
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "terraform-bucket-backend-lab1"
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    events              = ["s3:ObjectCreated:Put"]
+    filter_suffix       = ".tfstate"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
